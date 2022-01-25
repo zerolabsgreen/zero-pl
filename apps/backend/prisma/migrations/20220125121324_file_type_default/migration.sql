5825561583fde24e6e6a60558625fd035e8533cf
Joseph Bagaric <bagaricjos@gmail.com>
2022-01-25 14:31:12 +0100

    chore(backend): fix filetype migration

--- apps/backend/prisma/migrations/20220125121324_file_type_default/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "File" ALTER COLUMN "fileType" SET DEFAULT E'ATTESTATION';
--- apps/backend/prisma/migrations/20220125132421_file_type/migration.sql
+++ apps/backend/prisma/migrations/20220125132421_file_type/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - A unique constraint covering the columns `[purchaseId,fileType]` on the table `File` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `fileType` to the `File` table without a default value. This is not possible if the table is not empty.
   - Made the column `purchaseId` on table `File` required. This step will fail if there are existing NULL values in that column.
 
 */
@@ -16,7 +15,7 @@ ALTER TABLE "File" DROP CONSTRAINT "File_purchaseId_fkey";
 DROP INDEX "File_purchaseId_key";
 
 -- AlterTable
-ALTER TABLE "File" ADD COLUMN     "fileType" "FileType" NOT NULL,
+ALTER TABLE "File" ADD COLUMN     "fileType" "FileType" NOT NULL DEFAULT E'ATTESTATION',
 ALTER COLUMN "purchaseId" SET NOT NULL;
 
 -- CreateIndex
