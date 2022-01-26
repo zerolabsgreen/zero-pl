0580d602810ee4cbd98562248a48e5f9768df3f9
Joseph Bagaric <bagaricjos@gmail.com>
2022-01-26 22:07:41 +0100

    chore(backend): fix file migration

--- apps/backend/prisma/migrations/20220126140024_files_on_purchases/migration.sql
+++ apps/backend/prisma/migrations/20220126140024_files_on_purchases/migration.sql
@@ -4,15 +4,6 @@
   - You are about to drop the column `purchaseId` on the `File` table. All the data in the column will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE "File" DROP CONSTRAINT "File_purchaseId_fkey";
-
--- DropIndex
-DROP INDEX "File_purchaseId_fileType_key";
-
--- AlterTable
-ALTER TABLE "File" DROP COLUMN "purchaseId";
-
 -- CreateTable
 CREATE TABLE "FilesOnPurchases" (
     "fileId" TEXT NOT NULL,
@@ -26,3 +17,22 @@ ALTER TABLE "FilesOnPurchases" ADD CONSTRAINT "FilesOnPurchases_fileId_fkey" FOR
 
 -- AddForeignKey
 ALTER TABLE "FilesOnPurchases" ADD CONSTRAINT "FilesOnPurchases_purchaseId_fkey" FOREIGN KEY ("purchaseId") REFERENCES "Purchase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_purchaseId_fkey";
+
+-- DropIndex
+DROP INDEX "File_purchaseId_fileType_key";
+
+DO $$
+DECLARE
+file_ids record;
+BEGIN
+FOR file_ids IN SELECT id, "purchaseId" FROM "File"
+	LOOP
+	INSERT INTO "FilesOnPurchases" ("fileId", "purchaseId") VALUES (file_ids.id, file_ids."purchaseId");
+	END LOOP;
+END$$;
+
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "purchaseId";
