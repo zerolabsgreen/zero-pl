795d7d6e9b5734497d1f7aefb2449d824e841d4e
Joseph Bagaric <bagaricjos@gmail.com>
2022-01-26 22:34:13 +0100

    chore: temporarily disable https redirects on backend and issuer-api

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -68,7 +68,7 @@ import { ContractsModule } from '../contracts/contracts.module';
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(HttpLoggerMiddleware, HttpsRedirectMiddleware)
+      .apply(HttpLoggerMiddleware) //, HttpsRedirectMiddleware) - Disabling for now, doesn't work as expected
       .forRoutes('*');
   }
 }
